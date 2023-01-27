adb6c96f4a9643c052df70a6036638c3be7e25a0
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-01-27 02:01:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const btn = document.querySelector('button');
 const input = document.querySelector('#email');
+const error = document.querySelector('.warning');
 
-let inputMsg = input.value;
-
-if (inputMsg = "") {
-    btn.addEventListener('click', () => {
-        prompt('input something!');
-    })
-}
+btn.addEventListener('click', ()=> {
+    const err = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!err.test(input.value.toLowerCase())){
+        error.style.display = 'block';
+        input.style.borderColor = 'hsl(354, 100%, 66%)';
+    } else {
+        error.style.display = 'none';
+        input.style.borderColor = 'hsl(223, 100%, 88%)'
+    }
+    input.value = '';
+})
